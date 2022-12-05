27e9bb8e37738a6b926c8363118e976a22bd9eca
Magesh Kuppan <tkmagesh77@gmail.com>
2022-12-05 11:49:03 +0530

    intro

---
+++ 01-intro/01-hello-world.go
@@ -0,0 +1,19 @@
+/* package declaration (mandatory) */
+// Every go program must belong to a package
+// The starting point of the application must belong to the main package
+package main
+
+/* import dependency packages */
+import "fmt"
+
+/* package scoped type & variable declaration */
+
+/* package init function */
+
+/* main function (if this file is the starting point of the application) */
+func main() {
+	// print("Hello World!\n")
+	fmt.Println("Hello World!")
+}
+
+/* other functions */
