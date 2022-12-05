fc13b535d21b9c02a3c1fd70f7a47b6d46f45dee
Magesh Kuppan <tkmagesh77@gmail.com>
2022-12-05 14:12:38 +0530

    if else construct

---
+++ 02-programming-constructs/program.go
@@ -0,0 +1,23 @@
+/* programming constructs */
+package main
+
+import "fmt"
+
+func main() {
+	/* if else */
+	/*
+		no := 21
+		if no%2 == 0 {
+			fmt.Printf("%d is an even number\n", no)
+		} else {
+			fmt.Printf("%d is an odd number\n", no)
+		}
+	*/
+
+	if no := 21; no%2 == 0 { // the scope the variable is limited to the if-else block
+		fmt.Printf("%d is an even number\n", no)
+	} else {
+		fmt.Printf("%d is an odd number\n", no)
+	}
+
+}
