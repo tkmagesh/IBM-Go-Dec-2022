cd3788555713cd6891d2238a73234143c733cf51
Magesh Kuppan <tkmagesh77@gmail.com>
2022-12-06 09:47:22 +0530

    assignment-03 solution

--- 03-functions/assignment-03.go
+++ 03-functions/assignment-03.go
@@ -1,3 +1,71 @@
 /*
 Refactor the solution for assignment-02 with functions (every function should have ONLY one reason to change)
 */
+package main
+
+import "fmt"
+
+func main() {
+	var userChoice, n1, n2 int
+	for {
+		userChoice = getUserChoice()
+		if userChoice == 5 {
+			break
+		}
+		if userChoice < 1 || userChoice > 5 {
+			fmt.Println("Invalid choice")
+			continue
+		}
+		n1, n2 = getOperands()
+		processUserChoice(userChoice, n1, n2)
+	}
+}
+
+func getUserChoice() int {
+	var userChoice int
+	fmt.Println("1. Add")
+	fmt.Println("2. Subtract")
+	fmt.Println("3. Multiply")
+	fmt.Println("4. Divide")
+	fmt.Println("5. Exit")
+	fmt.Println("Enter your choice :")
+	fmt.Scanln(&userChoice)
+	return userChoice
+}
+
+func getOperands() (n1, n2 int) {
+	fmt.Println("Enter the nos:")
+	fmt.Scanln(&n1, &n2)
+	return
+}
+
+func processUserChoice(userChoice, n1, n2 int) {
+	var result int
+	switch userChoice {
+	case 1:
+		result = add(n1, n2)
+	case 2:
+		result = subtract(n1, n2)
+	case 3:
+		result = multiply(n1, n2)
+	case 4:
+		result = divide(n1, n2)
+	}
+	fmt.Println("Result :", result)
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+func subtract(x, y int) int {
+	return x - y
+}
+
+func multiply(x, y int) int {
+	return x * y
+}
+
+func divide(x, y int) int {
+	return x / y
+}
