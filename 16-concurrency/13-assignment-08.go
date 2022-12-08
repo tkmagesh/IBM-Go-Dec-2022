b29bcfb6ed67254004cb59654d8afd1b3136a280
Magesh Kuppan <tkmagesh77@gmail.com>
2022-12-08 14:05:23 +0530

    assignment-08 - problem statement

---
+++ 16-concurrency/13-assignment-08.go
@@ -0,0 +1,10 @@
+package main
+
+/*
+Write a go routine that will generate prime numbers between the given start and end
+Print the prime numbers in the main function as and when they are generated
+*/
+
+func main() {
+
+}
