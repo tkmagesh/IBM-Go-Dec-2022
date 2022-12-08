c3f5aa4602aab71c1e1bdf88d248ed76bf43fbf8
Magesh Kuppan <tkmagesh77@gmail.com>
2022-12-08 12:29:25 +0530

    more on channels

---
+++ 16-concurrency/08-demo-d.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+//share memory by communicating
+
+//Consumer
+func main() {
+	ch := add(100, 200)
+	result := <-ch
+	fmt.Println("Result :", result)
+}
+
+//Producer
+func add(x, y int) <-chan int {
+	ch := make(chan int)
+	go func() {
+		result := x + y
+		ch <- result
+	}()
+	return ch
+}
