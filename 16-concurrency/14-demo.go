e0d8ae97ef757c1b3131cacad0556bc3d03a8090
Magesh Kuppan <tkmagesh77@gmail.com>
2022-12-08 15:00:23 +0530

    using "select case" with multiple channels

--- 16-concurrency/14-demo.go
+++ 16-concurrency/14-demo.go
@@ -10,18 +10,49 @@ import (
 func main() {
 	ch1 := make(chan int)
 	ch2 := make(chan int)
-
+	ch3 := make(chan int)
 	go func() {
-		time.Sleep(5 * time.Second)
+		time.Sleep(2 * time.Second)
 		ch1 <- 100
 	}()
 
 	go func() {
-		time.Sleep(2 * time.Second)
+		time.Sleep(3 * time.Second)
+		data3 := <-ch3
+		fmt.Println("ch3 :", data3)
+	}()
+
+	go func() {
+		time.Sleep(5 * time.Second)
 		ch2 <- 200
 	}()
 
-	fmt.Println("ch1 :", <-ch1)
-	fmt.Println("ch2 :", <-ch2)
+	/*
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			fmt.Println("ch1 :", <-ch1)
+			wg.Done()
+		}()
+
+		wg.Add(1)
+		go func() {
+			fmt.Println("ch2 :", <-ch2)
+			wg.Done()
+		}()
+		wg.Wait()
+	*/
+	for i := 0; i < 3; i++ {
+		select {
+		case data1 := <-ch1:
+			fmt.Println("ch1 :", data1)
+		case ch3 <- 300:
+			fmt.Println("data sent to ch3")
+		case data2 := <-ch2:
+			fmt.Println("ch2 :", data2)
+			/* default:
+			fmt.Println("No channel operations succeeded") */
+		}
+	}
 
 }
