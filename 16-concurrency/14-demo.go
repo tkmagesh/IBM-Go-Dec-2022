3134b41f88923c4c1908acfa3dcbb1e28116b721
Magesh Kuppan <tkmagesh77@gmail.com>
2022-12-08 14:37:16 +0530

    handling multiple channels

---
+++ 16-concurrency/14-demo.go
@@ -0,0 +1,27 @@
+/* Using multiple channels */
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		ch1 <- 100
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch2 <- 200
+	}()
+
+	fmt.Println("ch1 :", <-ch1)
+	fmt.Println("ch2 :", <-ch2)
+
+}
